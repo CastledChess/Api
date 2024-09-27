076f8d8037d91055796b212e33c41680c246cee4
JohnARI <john.aristosa@hotmail.com>
2024-09-27 11:59:57 +0200

    chore(i18n): Setup i18n

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as path from 'path';
+import { I18nModule } from 'nestjs-i18n';
 
 @Module({
-  imports: [],
+  imports: [
+    I18nModule.forRoot({
+      fallbackLanguage: 'fr',
+      loaderOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: true,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
