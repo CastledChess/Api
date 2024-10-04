520b768c2081dbb68ba3421b02be7e4cab3a3751
JohnARI <john.aristosa@hotmail.com>
2024-10-04 12:14:00 +0200

    feat(i18n): Add i18n configuration files
    This commit adds the necessary configuration files for internationalization (i18n) support. It includes the i18n.config.ts file, which sets up the i18n options such as the fallback language and the path to the translation files. It also adds the i18n.constants.ts file, which defines constants used in the i18n configuration.
    
    Signed-off-by: JohnARI <john.aristosa@hotmail.com>

---
+++ src/i18n/i18n.config.ts
@@ -0,0 +1,12 @@
+import * as path from 'path';
+import { AcceptLanguageResolver, I18nOptions, QueryResolver } from 'nestjs-i18n';
+import { I18N_CONSTANTS } from './i18n.constants';
+
+export const i18nConfig: I18nOptions = {
+  fallbackLanguage: I18N_CONSTANTS.FALLBACK_LANGUAGE,
+  loaderOptions: {
+    path: path.join(__dirname, I18N_CONSTANTS.I18N_PATH),
+    watch: true,
+  },
+  resolvers: [{ use: QueryResolver, options: ['lang'] }, AcceptLanguageResolver],
+};
