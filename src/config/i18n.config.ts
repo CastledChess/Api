bab9ebd8c9a60b6fa966d1ed638ae74594ba0861
JohnARI <john.aristosa@hotmail.com>
2024-10-11 09:57:30 +0200

    chore(config): Update i18n config path

--- src/config/i18n.config.ts
+++ src/config/i18n.config.ts
@@ -4,7 +4,7 @@ import { AcceptLanguageResolver, I18nOptions, QueryResolver } from 'nestjs-i18n'
 export const i18nConfig: I18nOptions = {
   fallbackLanguage: 'fr',
   loaderOptions: {
-    path: path.join(__dirname),
+    path: path.join(__dirname, '../i18n/'),
     watch: true,
   },
   resolvers: [{ use: QueryResolver, options: ['lang'] }, AcceptLanguageResolver],
