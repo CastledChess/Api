5c44c77ba4ba44494bbed846fec529f2b912aa5b
JohnARI <john.aristosa@hotmail.com>
2024-10-11 09:55:17 +0200

    chore(docker): Update Dockerfile to optimize package installation

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,9 @@
+import { DocumentBuilder } from '@nestjs/swagger';
+import * as packageJson from '../../package.json';
+
+export const swaggerConfig = new DocumentBuilder()
+  .setTitle(packageJson.name)
+  .setDescription(packageJson.description)
+  .setVersion(packageJson.version)
+  .addTag('API')
+  .build();
