c02478f9a29bfb55a7b51d75bafb581e98bdb276
JohnARI <john.aristosa@hotmail.com>
2024-10-11 16:32:08 +0200

    fix: Replace i18n config to its folder

---
+++ src/i18n/i18n.config.ts
@@ -0,0 +1,11 @@
+import * as path from 'path';
+import { AcceptLanguageResolver, I18nOptions, QueryResolver } from 'nestjs-i18n';
+
+export const i18nConfig: I18nOptions = {
+  fallbackLanguage: 'fr',
+  loaderOptions: {
+    path: path.join(__dirname),
+    watch: true,
+  },
+  resolvers: [{ use: QueryResolver, options: ['lang'] }, AcceptLanguageResolver],
+};
