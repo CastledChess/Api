d13659ddd84a4990a415dd7d8d3cf80a28f17531
JohnARI <john.aristosa@hotmail.com>
2024-10-17 10:43:07 +0200

    feat(auth): Add Login and Register method to controller

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UsePipes, ValidationPipe } from '@nestjs/common';
+import { AuthenticationService } from './authentication.service';
+import { LoginDto } from './dto/login.dto';
+import { CreateUserDto } from './dto/create-user.dto';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('auth')
-export class AuthenticationController {}
+export class AuthenticationController {
+  constructor(private authService: AuthenticationService) {}
+
+  @Post('login')
+  @UsePipes(new ValidationPipe())
+  @ApiOperation({ summary: "Connexion de l'utilisateur" })
+  @ApiBody({ type: LoginDto })
+  @ApiResponse({ status: 200, description: 'Connexion réussie.' })
+  @ApiResponse({ status: 401, description: 'Identifiants invalides.' })
+  async login(@Body() loginDto: LoginDto) {
+    return this.authService.validateUser(loginDto);
+  }
+
+  @Post('register')
+  @UsePipes(new ValidationPipe())
+  @ApiOperation({ summary: "Inscription de l'utilisateur" })
+  @ApiBody({ type: CreateUserDto })
+  @ApiResponse({ status: 201, description: 'Inscription réussie.' })
+  @ApiResponse({ status: 409, description: "L'utilisateur existe déjà." })
+  async signUp(@Body() createUserDto: CreateUserDto) {
+    return this.authService.register(createUserDto);
+  }
+}
--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { User } from '../users/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
-export class AuthenticationService {}
+export class AuthenticationService {
+  constructor(private usersService: UsersService) {}
+
+  async validateUser(loginDto: LoginDto): Promise<User | null> {
+    const user = await this.usersService.findOne(loginDto.email);
+    if (user && user.password === loginDto.password) {
+      return user;
+    }
+    return null;
+  }
+
+  async register(createUserDto: CreateUserDto): Promise<User> {
+    return this.usersService.create(createUserDto);
+  }
+}
