638b236f4234e531ae5ed9417cf153f531228a32
JohnARI <john.aristosa@hotmail.com>
2024-10-17 10:42:22 +0200

    chore(auth): Update user and login DTOs

--- src/authentication/dto/create-user.dto.ts
+++ src/authentication/dto/create-user.dto.ts
@@ -3,19 +3,18 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
   @ApiProperty({ description: 'The email of the user', example: 'user@example.com' })
-  @Is
+  @IsString()
   email: string;
 
   @ApiProperty({ description: 'The username of the user', example: 'john_doe' })
   @IsString()
   username: string;
 
-  @ApiProperty({ description: 'The password of the user', example: 'strongPassword123' })
+  @ApiProperty({ description: 'The password of the user', example: 'password123' })
   @IsString()
   password: string;
 
-//   confirm password
-  @ApiProperty({ description: 'The confirm password of the user', example: 'strongPassword123' })
+  @ApiProperty({ description: 'The confirm password of the user', example: 'password123' })
   @IsString()
   confirmPassword: string;
 }
--- src/authentication/dto/login.dto.ts
+++ src/authentication/dto/login.dto.ts
@@ -1,9 +1,12 @@
 import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
+  @ApiProperty({ description: 'The email of the user', example: 'user@example.com' })
   @IsString()
   email: string;
 
+  @ApiProperty({ description: 'The password of the user', example: 'password123' })
   @IsString()
   password: string;
 }
