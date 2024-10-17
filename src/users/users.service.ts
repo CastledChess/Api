cdd894d38d280cffcde72e498c05a87e983ef2ed
JohnARI <john.aristosa@hotmail.com>
2024-10-17 10:37:11 +0200

    feat(users): Add create and findOne method to UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from '../authentication/dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const existingUser = await this.findOne(createUserDto.email);
+    if (existingUser) {
+      throw new ConflictException('User already exists');
+    }
+
+    const user = this.usersRepository.create({ ...createUserDto });
+    return this.usersRepository.save(user);
+  }
+
+  async findOne(email: string): Promise<User | undefined> {
+    return this.usersRepository.findOne({ where: { email } });
+  }
+}
