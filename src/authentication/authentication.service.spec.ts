d8c521c243c4c594ae0dd2b91b62b25d14332c33
JohnARI <john.aristosa@hotmail.com>
2024-10-24 13:34:31 +0200

    refactor(authentication): Update authentication service test setup

--- src/authentication/authentication.service.spec.ts
+++ src/authentication/authentication.service.spec.ts
@@ -1,12 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthenticationService } from './authentication.service';
+import { I18nService } from 'nestjs-i18n';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AuthenticationService', () => {
   let service: AuthenticationService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthenticationService],
+      providers: [
+        AuthenticationService,
+        {
+          provide: UsersService,
+          useValue: {},
+        },
+        {
+          provide: JwtService,
+          useValue: {},
+        },
+        {
+          provide: 'UserRepository',
+          useValue: {},
+        },
+        {
+          provide: I18nService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<AuthenticationService>(AuthenticationService);
