d1366739d920e48cf1995ec9c51aa6289bc1cb4d
JohnARI <john.aristosa@hotmail.com>
2024-10-31 12:06:55 +0100

    feat(authentication): Add validation to the login and register DTO's

--- src/authentication/dto/request/create-user.dto.ts
+++ src/authentication/dto/request/create-user.dto.ts
@@ -1,20 +1,25 @@
-import { IsString } from 'class-validator';
+import { IsString, IsStrongPassword, IsDefined, IsEmail } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { i18nValidationMessage } from 'nestjs-i18n';
 
 export class CreateUserDto {
   @ApiProperty({ description: 'The email of the user', example: 'user@example.com' })
-  @IsString()
+  @IsEmail()
+  @IsDefined()
   email: string;
 
   @ApiProperty({ description: 'The username of the user', example: 'john_doe' })
   @IsString()
+  @IsDefined()
   username: string;
 
   @ApiProperty({ description: 'The password of the user', example: 'password123' })
-  @IsString()
+  @IsStrongPassword({}, { message: i18nValidationMessage('validation.INVALID_PASSWORD') })
+  @IsDefined()
   password: string;
 
   @ApiProperty({ description: 'The confirm password of the user', example: 'password123' })
-  @IsString()
+  @IsStrongPassword({}, { message: i18nValidationMessage('validation.INVALID_PASSWORD') })
+  @IsDefined()
   confirmPassword: string;
 }
