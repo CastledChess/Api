c42061c0f16ef8ae4b90b1cbd378f7f983bc823b
Ludovic <ludovicdebever0@gmail.com>
2025-02-28 19:36:22 +0100

    fix(elitedb): fix config for prod

--- src/database/elite-database.config.ts
+++ src/database/elite-database.config.ts
@@ -4,6 +4,12 @@ import { DATABASE_TYPE } from './database.constants';
 export const getEliteDatabaseConfig = (): TypeOrmModuleOptions => ({
   type: DATABASE_TYPE,
   url: process.env.ELITE_DATABASE_URL,
+  // ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
   entities: [__dirname + '/../elitedb/entities/*.entity.{js,ts}'],
   // Si on est en production, on ne veut pas synchroniser pour éviter de perdre des données
   synchronize: false,
